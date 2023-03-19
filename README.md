d654468cfb0ff0e8cfa143ba6022e86bfdc3d16c
Luiz Fernando Mello <luizf.mello@hotmail.com>
2023-03-19 13:46:36 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# TCC_SSD_YOLO
---
+++ YOLO/execute_test.sh
@@ -0,0 +1,1 @@
+python test.py --data "data/pothole.yaml" --img 300 --batch 16 --conf 0.001 --iou 0.5 --device 0 --weights "runs/train/yolov7_pothole/weights/epoch_299.pt" --name yolov7_pothole > output_test.log
